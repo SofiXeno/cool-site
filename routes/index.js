5af1bcb4d8da7a75756085fe3c2220025331fe1e
Sofi Xeno <46944287+SofiXeno@users.noreply.github.com>
2021-04-17 21:28:24 +0300

    start of localization process

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const config = require('../config.json')
+const ua = require('../locales/ua.json')
+const en = require('../locales/en.json')
+
+function selectLang(lang){
+  switch (lang){
+    case "ua": return "ua"
+    case "en": return "en"
+    default: return "en"
+  }
+
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.ejs');
+  const lan = selectLang(req.params.lang)
+  res.render('index.ejs',{menuTabs:config.menuTabs, lang: lan});
 });
 
 
--- routes/trainings.js
+++ routes/trainings.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const config = require('../config.json')
+const ua = require('../locales/ua.json')
+const en = require('../locales/en.json')
+
+function selectLang(lang){
+    switch (lang){
+        case "ua": return "ua"
+        case "en": return "en"
+        default: return "en"
+    }
+
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('trainings.ejs');
+    const lan = selectLang(req.params.lang)
+    res.render('trainings.ejs',{menuTabs:config.menuTabs, lang: lan});
 });
 
+router.get('/nav', function(req, res, next) {
+    const lan = selectLang(req.params.lang)
+    res.render('nav.ejs',{menuTabs:config.menuTabs, lang: lan});
+});
+
+router.get('/form', function(req, res, next) {
+    const lan = selectLang(req.params.lang)
+    res.render('form.ejs',{menuTabs:config.menuTabs, lang: lan});
+});
 
 module.exports = router;
